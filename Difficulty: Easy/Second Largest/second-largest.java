744a8d3b5706d5a1d6496fbc60741435b8c92d71
Thenmugilan-Dom <thenmugilanks65@gmail.com>
2025-05-17 05:43:26 +0530

    Update second-largest.java

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -26,20 +26,6 @@ public class Main {
 
 class Solution {
     public int getSecondLargest(int[] arr) {
-        // int max1 = arr[0];
-        // int max2 = Integer.MIN_VALUE;
-        // for(int i = 1; i < arr.length; i ++){
-        //     if(arr[i] > max1){
-        //         max2 = max1;
-        //         max1 = arr[i];
-        //     }else if(arr[i] > max2 && arr[i] != max1){
-        //         max2 = arr[i];
-        //     }
-        // }
-        // if(max1 == max2 || max2 == Integer.MIN_VALUE ){
-        //     return -1;
-        // }
-        // return max2;
         
         int max1= arr[0];
         int max2 =Integer.MIN_VALUE;
