bf79f21811215a2c45fec93118e190f825f654fd
Thenmugilan-Dom <thenmugilanks65@gmail.com>
2025-06-13 22:39:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort The Array/sort-the-array.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+class Solution {
+    void sortArr(int[] arr) {
+        // code here
+        // int n = arr.length;
+        // for (int i = 0; i < n - 1; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (arr[i] > arr[j]) {
+        //             int temp = arr[i];
+        //             arr[i] = arr[j];
+        //             arr[j] = temp;
+        //         }
+        //     }
+        // }
+        
+        Arrays.sort(arr);
+    }
+}
