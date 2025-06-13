b2dc28c4218bafe10a3e134c21d33b6a0f81e047
Thenmugilan-Dom <thenmugilanks65@gmail.com>
2025-06-13 12:25:58 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (41.99%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int a = 0;
+        int b = 0;
+        for(int i : nums){
+            a = (a^i) & ~b;
+            b = (b^i) & ~a;
+        }
+        return a;
+    }
+}
