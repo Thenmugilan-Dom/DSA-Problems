c4e572b73a2246442638d6742a56ba06dca692e9
Thenmugilan K S <thenmugilanks65@gmail.com>
2025-10-28 21:16:57 +0530

    Time: 100 ms (23.49%) | Memory: 43.5 MB (13.25%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countValidSelections(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                if (simulate(nums.clone(), i, 1)) count++;
+                if (simulate(nums.clone(), i, -1)) count++;
+            }
+        }
+
+        return count;
+    }
+
+    private boolean simulate(int[] arr, int curr, int dir) {
+        int n = arr.length;
+
+        while (curr >= 0 && curr < n) {
+            if (arr[curr] == 0) {
+                curr += dir; 
+            } else {
+                arr[curr]--; 
+                dir = -dir;  
+                curr += dir;  
+            }
+        }
+
+        for (int num : arr) {
+            if (num != 0) return false;
+        }
+
+        return true;
+    }
+}
